62b7cbb5ea18424cd2aa7920d281314c1a978629
Zishan <zishansiddique585@gmail.com>
2022-07-25 18:58:01 +0545

    loggin in works fine

--- src/App.js
+++ src/App.js
@@ -1,24 +1,97 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
 
-  return (
-    <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
-    </div>
-  )
-}
+  useEffect(() => {
+    const loggedUser = window.localStorage.getItem("loggedUser");
+    if (loggedUser) {
+      const user = JSON.parse(loggedUser);
+      setUser(user);
+    }
+  }, []);
 
-export default App
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    try {
+      const user = await loginService.login({ username, password });
+      window.localStorage.setItem("loggedUser", JSON.stringify(user));
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    const name = event.target.name;
+    const value = event.target.value;
+    if (name === "Username") {
+      setUsername(value);
+    } else if (name === "Password") {
+      setPassword(value);
+    }
+  };
+
+  const condRendering = () => {
+    if (user === null) {
+      return (
+        <div>
+          <h2>Log in to application</h2>
+          <form onSubmit={handleLogin}>
+            <label htmlFor="username">username</label>
+            <input
+              id="username"
+              type="text"
+              value={username}
+              name="Username"
+              onChange={handleChange}
+            />
+            <label htmlFor="password">password</label>
+            <input
+              id="password"
+              type="password"
+              value={password}
+              name="Password"
+              onChange={handleChange}
+            />
+            <button type="submit">login</button>
+          </form>
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        <h2>blogs</h2>
+        {blogs.map((blog) => (
+          <Blog key={blog.id} blog={blog} />
+        ))}
+        <button
+          onClick={(event) => {
+            event.preventDefault();
+            window.localStorage.removeItem("loggedUser");
+            window.location.reload();
+          }}
+        >
+          logout
+        </button>
+      </div>
+    );
+  };
+  return <>{condRendering()}</>;
+};
+
+export default App;
